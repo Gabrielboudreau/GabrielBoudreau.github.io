5deb5c32eb6983be0710b3d62514a1fb03090f45
Gabriel Boudreau <89223461+Gabrielboudreau@users.noreply.github.com>
2022-03-22 20:54:05 +0000

    Embed

--- lists.py
+++ lists.py
@@ -1,1 +1,106 @@
+InfoDb = []
+# List with dictionary records placed in a list  
+InfoDb.append({  
+               "FirstName": "Gabriel",  
+               "LastName": "Boudreau",  
+               "DOB": "January 25",  
+               "Residence": "San Diego",  
+               "Email": "gabrielb50533@gmail.com",  
+               "Pets":["Molly, Obi, Jax, Zoey, Dragon"]  
+              })  
+
+InfoDb.append({  
+               "FirstName": "Bob",  
+               "LastName": "Ross",  
+               "DOB": "October 29",  
+               "Residence": "Florida",  
+               "Email": "calminghillside@gmail.com",  
+               "Pets":["Annete, Bob, Bobette"]  
+              })
+
+InfoDb.append({  
+               "FirstName": "Ryan",  
+               "LastName": "Reynolds",  
+               "DOB": "October 23",  
+               "Residence": "Los Angeles",  
+               "Email": "ryansleftpec@gmail.com",  
+               "Pets":["Penny, Baxter"]  
+              })
+
+InfoDb.append({  
+               "FirstName": "Elon",  
+               "LastName": "Musk",  
+               "DOB": "June 28",  
+               "Residence": "Philidelphia",  
+               "Email": "cryptocrash@gmail.com",  
+               "Pets":["Floki"]
+              })
+
+# given an index this will print InfoDb content
+def print_data(n):
+    print(InfoDb[n]["FirstName"], InfoDb[n]["LastName"])  # using comma puts space between values
+    print("\t", "Pets: ", end="")  # \t is a tab indent, end="" make sure no return occurs
+    print(", ".join(InfoDb[n]["Pets"]))  # join allows printing a string list with separator
+    print()
+
+# Hack 2: InfoDB loops. Print values from the lists using three different ways: for, while, recursion
+## hack 2a: def for_loop()
+## hack 2b: def while_loop(0)
+## hack 2c : def recursive_loop(0)
+
+
+
+def for_loop():
+    for n in range(len(InfoDb)):
+        print_data(n)
+
+def tester():
+    print("For loop")
+    for_loop()
+    print("While loop")
+    while_loop(0)  # requires initial index to start while
+    print("Recursive loop")
+    recursive_loop(0)  # requires initial index to start recursion
+
+def while_loop(n):
+    while n < len(InfoDb):
+        print_data(n)
+        n += 1
+    return
+
+def recursive_loop(n):
+    if n < len(InfoDb):
+        print_data(n)
+        recursive_loop(n + 1)
+    return # exit condition
+
+# Factorial of a number using recursion
+def recur_factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * recur_factorial(n-1)
+
+# this is test driver or code that plays when executed directly, versus import which will not run these statements
+def tester1():
+    num = int(input("Enter a number for factorial: "))
+    # check if the number is negative
+    if num < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+    else:
+        print("The factorial of", num, "is", recur_factorial(num))
+def fibonacci():
+  num = int(input("Enter sequence length: "))
+  if num < 0:
+    print("Enter a number greater than 0")
+  else:
+    for n in range(num):
+      print(print_fib(n))
+
+def print_fib(i):
+  if i <= 1:  
+     return i  
+  else:  
+     return(print_fib(i-2) + print_fib(i-1))
+
 
--- main.py
+++ main.py
@@ -1,1 +1,144 @@
+import time
+import lists
+def menu():
+    print('1 -- Swap Numbers' )
+    print('2 -- Animation' )
+    print('3 -- TT1')
+    print('4 -- Exit' )
+    runOptions()
 
+def sub_menu():
+    print('1 -- Bigger number First')
+    print('2 -- Smaller number First')
+    print('3 -- Return')
+    print('4 -- Exit')
+    subOptions()
+
+def sub_menu1():
+    print('1 -- Fibonacci')
+    print('2 -- Lists')
+    print('3 -- Exit')
+    suboptions1()
+
+def num_swap():
+    sub_menu()
+
+def animation():
+    ship()
+
+def swap():
+  while True:
+      try:
+          num1 = int(input('choose a number:  '))
+          num2 = int(input('choose a second number: '))
+          swapValues = [num1, num2]
+          if True:
+            swapValues.sort(reverse = True)
+            print(swapValues)
+            menu()
+          else:
+            swapValues.sort()
+            print(swapValues)
+            menu()
+      except ValueError:
+          print('Invalid input. Please enter an integer input.')
+    
+def suboptions1():
+    while True:
+        try:
+            option = int(input('Enter your choice 1-4: '))
+
+            if option == 1:
+              tester1()
+            elif option == 2:
+              tester()
+            elif option == 3:  
+                print('Goodbye!')
+                exit() 
+            else:
+                print('Invalid option. Please enter a number between 1 and 4.')
+        except ValueError:
+            print('Invalid input. Please enter an integer input.')
+
+def subOptions():
+    while True:
+        try:
+            option = int(input('Enter your choice 1-4: '))
+            bigger = False
+            if option == 1:
+                bigger == True
+                swap()
+            elif option == 2:
+                swap()
+            elif option == 3:
+                menu()
+            elif option == 4:  
+                print('Goodbye!')
+                exit() 
+            else:
+                print('Invalid option. Please enter a number between 1 and 4.')
+        except ValueError:
+            print('Invalid input. Please enter an integer input.')
+
+def runOptions():
+    while True:
+        try:
+            option = int(input('Enter your choice 1-3: '))
+            if option == 1:
+                num_swap()
+            elif option == 2:
+                animation()
+                menu()    
+            elif option == 3:
+              submenu1()
+            elif option == 4:  
+                print('Goodbye!')
+                exit() 
+            else:
+                print('Invalid option. Please enter a number between 1 and 4.')
+        except ValueError:
+            print('Invalid input. Please enter an integer input.')
+
+ANSI_CLEAR_SCREEN = u"\u001B[2J"
+ANSI_HOME_CURSOR = u"\u001B[0;0H\u001B[2"
+OCEAN_COLOR = u"\u001B[44m\u001B[2D"
+SHIP_COLOR = u"\u001B[32m\u001B[2D"
+RESET_COLOR = u"\u001B[0m\u001B[2D"
+
+def ocean_print():
+
+    print(ANSI_CLEAR_SCREEN, ANSI_HOME_CURSOR)
+    print("\n\n\n\n")
+    print(OCEAN_COLOR + "  " * 35)
+
+
+def ship_print(position):
+    print(ANSI_HOME_CURSOR)
+    print(RESET_COLOR)
+    sp = " " * position
+    print(sp + "  \_O   ")
+    print(sp + "    |\   ")
+    print(sp + "    |")
+    print(sp + "   / |")
+    print(sp + " \____/  ")
+    print(RESET_COLOR)
+
+
+
+def ship():
+
+    ocean_print()
+
+    start = 0  
+    distance = 60 
+    step = 2 
+
+   
+    for position in range(start, distance, step):
+        ship_print(position) 
+        time.sleep(.1)
+
+
+if __name__=='__main__':
+    menu()
+    
