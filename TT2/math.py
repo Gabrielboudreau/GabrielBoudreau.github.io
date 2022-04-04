528648f411ba8235866f757a3490d4bcd33ea792
Gabriel Boudreau <89223461+Gabrielboudreau@users.noreply.github.com>
2022-04-04 19:34:47 +0000

    Fixed math

--- TT2/math.py
+++ TT2/math.py
@@ -1,29 +1,5 @@
 import re
-
-
-
-# ---------- Class Factorial ----------
-
-class Factorial:
-  def __init__(self, num):
-    self.num = num 
-  def __call__(self):
-    if self.num == 1:
-      return self.num
-    if self.num < 0:
-      print("No factorial for this number")
-    else:
-      n = 1
-      for i in range (1,int(self.num)+1):
-        n = n*i
-        print(i, ":", n)
-      print("The factorial of", self.num, "is", n) 
-        
-def factorial_print():
-  inp = int(input("Enter a number you want the factorial of: "))
-  p1 = Factorial(inp)
-  p1()
-
+       
 def seq_sum():
   num = int(input("Enter a number for your sequential sum: "))
   n = 0
@@ -33,10 +9,6 @@ def seq_sum():
     for i in range(1, num + 1):
       n = n + i
     print("The sum of all numbers starting from 1 to", num, "is", n)
-
-
-
-# ---------- Class Sequential Numbers ----------
 
 class Consec_Nums:
   def __init__(self, num):
